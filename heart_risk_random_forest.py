c35aa59c9c20a613aa6f76abe6833f4b2895d532
jt1732 <jtur305@aucklanduni.ac.nz>
2025-08-05 15:11:17 +1200

    Added CSV file reading and Random Forest Features

--- heart_risk_random_forest.py
+++ heart_risk_random_forest.py
@@ -6,3 +6,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, roc_auc_score, precision_recall_curve
 import matplotlib.pyplot as plt
 import joblib
+
+df = pd.read_csv("")
+
+features = [
+    "Chest_Pain","Shortness_of_Breath","Fatigue","Palpitations",
+    "Dizziness","Swelling","Pain_Arms_Jaw_Back",
+    "Cold_Sweats_Nausea","High_BP","High_Cholesterol",
+    "Diabetes","Smoking","Obesity","Sedentary_Lifestyle",
+    "Family_History","Chronic_Stress","Gender","Age",
+]
