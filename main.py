7c84b63316173d880c9ccc6c64ae4fbff3c38cef
jt1732 <jtur305@aucklanduni.ac.nz>
2025-08-05 23:01:18 +1200

    Questions

--- main.py
+++ main.py
@@ -13,3 +13,24 @@ root = tk.Tk()
 root.title("Heart Risk Predictor")
 frm = tk.Frame(root, padx=10, pady=10)
 frm.grid()
+
+widgets = {
+    "chest_pain": ["experience chest pain"],
+    "shortness_of_breath": ["experience shortness of breath"],
+    "fatigue": ["experience fatigue"],
+    "palpitations": ["experience palpitations"],
+    "dizziness": ["experience dizziness"],
+    "swelling": ["experience swelling"],
+    "arm_jaw_back_pain": ["experience pain in your arms, jaw or back"],
+    "cold_sweats_nausea": ["experience cold sweats or nausea"],
+    "high_bp": ["experience high BP"],
+    "high_cholesterol": ["have high cholesterol"],
+    "diabetes": ["have diabetes"],
+    "smoking": ["smoke"],
+    "obesity": ["have obesity"],
+    "sedentary_lifestyle": ["have a sedentary lifestyle"],
+    "family_history": ["have a family history"],
+    "chronic_stress": ["experience chronic stress"]
+}
+
+root.mainloop()
