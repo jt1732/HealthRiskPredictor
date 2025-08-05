9d029af665bbe28a7b5b2e010c66ad5ec984d787
jt1732 <jtur305@aucklanduni.ac.nz>
2025-08-05 22:50:57 +1200

    Init OpenAi client

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import tkinter as tk
 import math
 import joblib
+import os
+from dotenv import load_dotenv
 from openai import OpenAI
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
